5d6ea946c3fc7dd43f686a477f5f139ae7b3ba3d
streetms <konstantin.isakov.2003@mail.ru>
2023-03-09 21:20:36 +0700

    функция Update::get больше не принимает строковый параметр

--- Update/Update.h
+++ Update/Update.h
@@ -6,15 +6,16 @@
 #define CPPGRAM_UPDATE_H
 
 #include "boost/property_tree/ptree.hpp"
-
+#include <boost/type_index.hpp>
 class Update {
 public:
-    explicit Update(const boost::property_tree::ptree& tree){
-        json = tree;
+    explicit Update(const boost::property_tree::ptree& tree) : json(tree){
     }
     template <typename T>
-    T get(std::string_view name){
-        return (json.get_child(name.data()));
+    T get(){
+        std::string type_name = boost::typeindex::type_id<T>().pretty_name();
+        type_name[0] = char(std::tolower(type_name[0]));
+        return json.get_child(type_name);
     }
 private:
     boost::property_tree::ptree json;
